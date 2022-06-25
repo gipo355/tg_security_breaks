2a72868bdce2f1f92c02d0c4d920dcb0e0fb8c28
gipo355 <gipo18649ah@gmail.com>
2022-06-25 23:55:06 +0200

    first push

--- src/app.ts
+++ src/app.ts
@@ -55,99 +55,133 @@ interface Security {
     };
 }
 
-const data = fs.readFileSync(path.join(__dirname, `/api/db.json`), 'utf-8');
-const security: Security = JSON.parse(data);
-// console.log(sec);
+// ! BUG IF I WANT TO CHANGE AND REMOVE A WORKER IT FUCKS UP EVERYTHING BECAUSE THEY ARE CONST,
+// ! got to fix both the interface and the initialize db below
+function writeTurns() {
+    const data = fs.readFileSync(
+        path.join(__dirname, '..', `/api/db.json`),
+        'utf-8'
+    );
+    const security: Security = JSON.parse(data);
+    // console.log(sec);
 
-// const security: Security = {
-//     workers: ['levante1', 'levante2', 'ponente1', 'ponente2'],
-//     levante1: {
-//         torrette: [9, 10, 11, 12, 13, 14],
-//         startingHour: 11,
-//     },
-//     levante2: {
-//         torrette: [15, 16, 17, 18, 19],
-//         startingHour: 12,
-//     },
-//     ponente1: {
-//         torrette: [28, 29, 30, 31, 32, 33],
-//         startingHour: 11,
-//     },
-//     ponente2: {
-//         torrette: [34, 35, 36, 37, 38, 39],
-//         startingHour: 11,
-//     },
-// };
+    // const security: Security = {
+    //     workers: ['levante1', 'levante2', 'ponente1', 'ponente2'],
+    //     levante1: {
+    //         torrette: [9, 10, 11, 12, 13, 14],
+    //         startingHour: 11,
+    //     },
+    //     levante2: {
+    //         torrette: [15, 16, 17, 18, 19],
+    //         startingHour: 12,
+    //     },
+    //     ponente1: {
+    //         torrette: [28, 29, 30, 31, 32, 33],
+    //         startingHour: 11,
+    //     },
+    //     ponente2: {
+    //         torrette: [34, 35, 36, 37, 38, 39],
+    //         startingHour: 11,
+    //     },
+    // };
 
-// ! objective, return a string
+    // ! objective, return a string
 
-function switchFirstToLast(arr: number[]) {
-    const lastTurret = arr.shift() as number;
-    arr.push(lastTurret);
-}
-
-function message(
-    _worker: typeof security.workers[number],
-    _startingHour: number
-) {
-    // set useful vars
-    const turretsArr = security[_worker].torrette;
-    const firstTurret = security[_worker].torrette[0];
-
-    // starting string
-    const str = `${_worker.slice(0, _worker.length - 1)} ${_worker.slice(
-        _worker.length - 1
-    )} inizia alla torretta ${firstTurret} alle ${_startingHour.toFixed(
-        2
-    )}`.toUpperCase();
-
-    // turns string
-    let startHour = _startingHour;
-    let turnsStr = '';
-    for (const turret of turretsArr) {
-        // console.log(turret);
-        turnsStr += `torretta ${turret}: ${startHour.toFixed(2)} -> ${(
-            startHour + 1
-        ).toFixed(2)}\n`.padStart(35);
-        startHour++;
+    function switchFirstToLast(arr: number[]) {
+        const lastTurret = arr.shift() as number;
+        arr.push(lastTurret);
     }
 
-    // console.log(str);
-    // console.log(turnsStr);
+    function message(
+        _worker: typeof security.workers[number],
+        _startingHour: number
+    ) {
+        // set useful vars
+        const turretsArr = security[_worker].torrette;
+        const firstTurret = security[_worker].torrette[0];
 
-    switchFirstToLast(turretsArr);
-    // console.log(turretsArr);
+        // starting string
+        const str = `${_worker.slice(0, _worker.length - 1)} ${_worker.slice(
+            _worker.length - 1
+        )} inizia alla torretta ${firstTurret} alle ${_startingHour.toFixed(
+            2
+        )}`.toUpperCase();
 
-    // return finalStr;
-    return `${str}\n${turnsStr}\n`;
+        // turns string
+        let startHour = _startingHour;
+        let turnsStr = '';
+        for (const turret of turretsArr) {
+            // console.log(turret);
+            turnsStr += `torretta ${turret}: ${startHour.toFixed(2)} -> ${(
+                startHour + 1
+            ).toFixed(2)}\n`.padStart(35);
+            startHour++;
+        }
+
+        // console.log(str);
+        // console.log(turnsStr);
+
+        switchFirstToLast(turretsArr);
+        // console.log(turretsArr);
+
+        // return finalStr;
+        return `${str}\n${turnsStr}\n`;
+    }
+
+    function allMessage() {
+        const date = new Date().toLocaleDateString('it-IT');
+        // console.log(date, '\n');
+        let finalStr = `${date}\n\n`;
+        for (const [i, worker] of security.workers.entries()) {
+            // console.log(security[`levante${i + 1}`].startingHour);
+            // console.log(i, worker);
+            // const beach = security[worker];
+            // console.log(beach.startingHour);
+
+            // console.log(security['levante1']);
+
+            const prop = security[worker];
+            // console.log(prop);
+            // console.log(prop.startingHour);
+            finalStr += message(worker, prop.startingHour);
+            // console.log(worker);
+        }
+        console.log(finalStr);
+        fs.writeFileSync(
+            path.join(__dirname, '..', '/api/db.json'),
+            JSON.stringify(security)
+        );
+        return finalStr;
+    }
+    return allMessage();
 }
 
-function allMessage() {
-    const date = new Date().toLocaleDateString('it-IT');
-    // console.log(date, '\n');
-    let finalStr = `${date}\n\n`;
-    for (const [i, worker] of security.workers.entries()) {
-        // console.log(security[`levante${i + 1}`].startingHour);
-        // console.log(i, worker);
-        // const beach = security[worker];
-        // console.log(beach.startingHour);
-
-        // console.log(security['levante1']);
-
-        const prop = security[worker];
-        // console.log(prop);
-        // console.log(prop.startingHour);
-        finalStr += message(worker, prop.startingHour);
-        // console.log(worker);
-    }
-    console.log(finalStr);
+function initializeDb() {
+    const security: Security = {
+        workers: ['levante1', 'levante2', 'ponente1', 'ponente2'],
+        levante1: {
+            torrette: [9, 10, 11, 12, 13, 14],
+            startingHour: 11,
+        },
+        levante2: {
+            torrette: [15, 16, 17, 18, 19],
+            startingHour: 12,
+        },
+        ponente1: {
+            torrette: [28, 29, 30, 31, 32, 33],
+            startingHour: 11,
+        },
+        ponente2: {
+            torrette: [34, 35, 36, 37, 38, 39],
+            startingHour: 11,
+        },
+    };
     fs.writeFileSync(
-        path.join(__dirname, '/api/db.json'),
+        path.join(__dirname, '..', '/api/db.json'),
         JSON.stringify(security)
     );
-    return finalStr;
 }
 
-allMessage();
-// allMessage();
-// allMessage();
+export { writeTurns as default, initializeDb };
+// writeTurns();
+// initializeDb();
--- src/index.ts
+++ src/index.ts
@@ -2,49 +2,53 @@
 
 import 'dotenv-defaults/config';
 import { scheduleJob, RecurrenceRule } from 'node-schedule';
-
 import { Telegraf, Telegram } from 'telegraf';
+import sendTurns, { initializeDb } from './app';
 
 const bot = new Telegraf(process.env.TG_KEY as string);
 const telegram = new Telegram(process.env.TG_KEY as string);
-
 const chatID = process.env.CHAT_ID as string;
 
 // BOT
-bot.start((ctx) => {
-    ctx.reply(`Hello  ${ctx.from.first_name}${ctx.from.last_name}!`);
-});
+// bot.start((ctx) => {
+//     ctx.reply(`Hello  ${ctx.from.first_name}${ctx.from.last_name}!`);
+// });
 
-bot.help((ctx) => ctx.reply('Send me a sticker'));
+// bot.help((ctx) => {
+//     ctx.reply('restartDB, start');
+// });
 
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+// bot.hears('restartDB', (ctx) => {
+//     initializeDb();
+//     ctx.reply('DB restarted');
+// });
 
-const tryDate = new Date().toLocaleString('it-IT', { timeZone: 'Europe/Rome' });
+// const tryDate = new Date().toLocaleString('it-IT', { timeZone: 'Europe/Rome' });
 // console.log(tryDate);
 
 // RECURRENT RULE - SCHEDULED
 const rule = new RecurrenceRule();
-rule.hour = 8;
+rule.hour = 7;
 // rule.minute = 44;
 rule.tz = `CET`;
 
-// const job = scheduleJob('*/1 * * * *', function () {
-//     console.log('Today is recognized by Rebecca Black!');
-//     telegram.sendMessage(chatID, `${new Date().toLocaleTimeString()}`);
-// });
+const job = scheduleJob(rule, function () {
+    // console.log('Today is recognized by Rebecca Black!');
+    // telegram.sendMessage(chatID, `${new Date().toLocaleTimeString()}`);
+    telegram.sendMessage(chatID, sendTurns());
+});
 
-function getDate() {
-    const date = new Date();
-    const str = date.toLocaleTimeString('it-IT');
-    return str;
-}
+// function getDate() {
+//     const date = new Date();
+//     const str = date.toLocaleTimeString('it-IT');
+//     return str;
+// }
 
 const job2 = scheduleJob('*/10 * * * * *', function () {
     // console.log('Today is recognized by Rebecca Black!');
     // console.log(tryDate);
-
     // telegram.sendMessage(chatID, `${new Date().toLocaleTimeString()}`);
-    telegram.sendMessage(chatID, getDate());
+    telegram.sendMessage(chatID, sendTurns());
 });
 
 // ! LAUNCH AND END GRACEFULLY ON NODE STOP
