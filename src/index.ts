5216c9d179713e6dc80c78668930853f745c651e
gipo355 <gipo18649ah@gmail.com>
2022-06-25 18:30:01 +0200

    first push

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
 'use strict';
 
 import 'dotenv-defaults/config';
+
 import { Telegraf, Telegram } from 'telegraf';
 
 const bot = new Telegraf(process.env.TG_KEY as string);
 const telegram = new Telegram(process.env.TG_KEY as string);
 
-const chatId = ''
 
 // bot.start((ctx) => ctx.reply('Welcome'));
 bot.start((ctx) => {
     ctx.reply(`Hello  ${ctx.from.first_name}${ctx.from.last_name}!`);
 });
 bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on('sticker', (ctx) => ctx.reply('👍'));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+
+const chatID = process.env.CHAT_ID as string;
+
+telegram.sendMessage(chatID, `${new Date().getTime()}`);
 
 // ! LAUNCH AND END GRACEFULLY ON NODE STOP
 bot.launch();
