c3f7176c19b1920156c48b7bcbc23ebcf17c4132
gipo355 <gipo18649ah@gmail.com>
2022-06-25 23:05:02 +0200

    first push

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 'use strict';
 
 import 'dotenv-defaults/config';
+import { scheduleJob, RecurrenceRule } from 'node-schedule';
 
 import { Telegraf, Telegram } from 'telegraf';
 
@@ -9,6 +10,7 @@ const telegram = new Telegram(process.env.TG_KEY as string);
 
 const chatID = process.env.CHAT_ID as string;
 
+// BOT
 bot.start((ctx) => {
     ctx.reply(`Hello  ${ctx.from.first_name}${ctx.from.last_name}!`);
 });
@@ -17,7 +19,33 @@ bot.help((ctx) => ctx.reply('Send me a sticker'));
 
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 
-telegram.sendMessage(chatID, `${new Date().getTime()}`);
+const tryDate = new Date().toLocaleString('it-IT', { timeZone: 'Europe/Rome' });
+// console.log(tryDate);
+
+// RECURRENT RULE - SCHEDULED
+const rule = new RecurrenceRule();
+rule.hour = 8;
+// rule.minute = 44;
+rule.tz = `CET`;
+
+// const job = scheduleJob('*/1 * * * *', function () {
+//     console.log('Today is recognized by Rebecca Black!');
+//     telegram.sendMessage(chatID, `${new Date().toLocaleTimeString()}`);
+// });
+
+function getDate() {
+    const date = new Date();
+    const str = date.toLocaleTimeString('it-IT');
+    return str;
+}
+
+const job2 = scheduleJob('*/10 * * * * *', function () {
+    // console.log('Today is recognized by Rebecca Black!');
+    // console.log(tryDate);
+
+    // telegram.sendMessage(chatID, `${new Date().toLocaleTimeString()}`);
+    telegram.sendMessage(chatID, getDate());
+});
 
 // ! LAUNCH AND END GRACEFULLY ON NODE STOP
 bot.launch();
