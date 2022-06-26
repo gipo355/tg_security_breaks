2bbb26e8c45da922496d4c43724b16e0a9df5214
gipo355 <gipo18649ah@gmail.com>
2022-06-26 21:22:38 +0200

    first push

--- src/app.ts
+++ src/app.ts
@@ -3,38 +3,6 @@
 import fs from 'fs';
 import path from 'path';
 
-/**
- *  bot sends message every day at 0800 with
- *
- *      1- current day
- *      2- beach: levante, ponente
- *      3- levante 1, levante 2 / ponente 1, ponente 2 hours per turret
- *
- *          return: where do turns start, at what hour, every position break
- *
- *      every day, levante 1 and levante 2 start + 1 position
- */
-
-/**
- *  example format:
- *
- *  25/06/2022
- *  LEVANTE 1 INIZIA ALLA TORRETTA 15 ALLE ORE 12:00
- *      torretta 15: 12.00 -> 13.00
- *      torretta 16: 13.00 -> 14.00
- *      torretta 17: 14.00 -> 15.00
- *      torretta 18: 15.00 -> 16.00
- *      torretta 19: 16.00 -> 17.00
-
- *  26/06/2022
- *  LEVANTE 1 INIZIA ALLA TORRETTA 16 ALLE ORE 12:00
- *      torretta 16: 12.00 -> 13.00
- *      torretta 17: 13.00 -> 14.00
- *      torretta 18: 14.00 -> 15.00
- *      torretta 18: 15.00 -> 16.00
- *      torretta 15: 16.00 -> 17.00
- */
-
 interface Security {
     workers: ['levante1', 'levante2', 'ponente1', 'ponente2'];
     levante1: {
@@ -58,34 +26,14 @@ interface Security {
 // ! BUG IF I WANT TO CHANGE AND REMOVE A WORKER IT FUCKS UP EVERYTHING BECAUSE THEY ARE CONST,
 // ! got to fix both the interface and the initialize db below
 function writeTurns() {
+    // get API db
     const data = fs.readFileSync(
         path.join(__dirname, '..', `/api/db.json`),
         'utf-8'
     );
+
+    // parse into obj
     const security: Security = JSON.parse(data);
-    // console.log(sec);
-
-    // const security: Security = {
-    //     workers: ['levante1', 'levante2', 'ponente1', 'ponente2'],
-    //     levante1: {
-    //         torrette: [9, 10, 11, 12, 13, 14],
-    //         startingHour: 11,
-    //     },
-    //     levante2: {
-    //         torrette: [15, 16, 17, 18, 19],
-    //         startingHour: 12,
-    //     },
-    //     ponente1: {
-    //         torrette: [28, 29, 30, 31, 32, 33],
-    //         startingHour: 11,
-    //     },
-    //     ponente2: {
-    //         torrette: [34, 35, 36, 37, 38, 39],
-    //         startingHour: 11,
-    //     },
-    // };
-
-    // ! objective, return a string
 
     function switchFirstToLast(arr: number[]) {
         const lastTurret = arr.shift() as number;
@@ -96,7 +44,6 @@ function writeTurns() {
         _worker: typeof security.workers[number],
         _startingHour: number
     ) {
-        // set useful vars
         const turretsArr = security[_worker].torrette;
         const firstTurret = security[_worker].torrette[0];
 
@@ -107,52 +54,44 @@ function writeTurns() {
             2
         )}`.toUpperCase();
 
-        // turns string
         let startHour = _startingHour;
         let turnsStr = '';
+
+        // string block per turret
         for (const turret of turretsArr) {
-            // console.log(turret);
             turnsStr += `torretta ${turret}: ${startHour.toFixed(2)} -> ${(
                 startHour + 1
             ).toFixed(2)}\n`.padStart(35);
             startHour++;
         }
 
-        // console.log(str);
-        // console.log(turnsStr);
-
+        // change db
         switchFirstToLast(turretsArr);
-        // console.log(turretsArr);
 
-        // return finalStr;
         return `${str}\n${turnsStr}\n`;
     }
 
     function allMessage() {
         const date = new Date().toLocaleDateString('it-IT');
-        // console.log(date, '\n');
-        let finalStr = `${date}\n\n`;
+
+        let finalStr = `🌊 PAUSE DEL ${date} 🌊\n\n`;
+
         for (const [i, worker] of security.workers.entries()) {
-            // console.log(security[`levante${i + 1}`].startingHour);
-            // console.log(i, worker);
-            // const beach = security[worker];
-            // console.log(beach.startingHour);
-
-            // console.log(security['levante1']);
-
             const prop = security[worker];
-            // console.log(prop);
-            // console.log(prop.startingHour);
+
             finalStr += message(worker, prop.startingHour);
-            // console.log(worker);
         }
+
         console.log(finalStr);
+
         fs.writeFileSync(
             path.join(__dirname, '..', '/api/db.json'),
             JSON.stringify(security)
         );
+
         return finalStr;
     }
+
     return allMessage();
 }
 
@@ -176,6 +115,7 @@ function initializeDb() {
             startingHour: 11,
         },
     };
+
     fs.writeFileSync(
         path.join(__dirname, '..', '/api/db.json'),
         JSON.stringify(security)
--- src/index.ts
+++ src/index.ts
@@ -9,52 +9,11 @@ const port = Number(process.env.PORT);
 const URL = process.env.URL;
 const chatID = process.env.CHAT_ID as string;
 
-// const options: Partial<Options<Context<Update>>> = {
-//     webHook: {
-//         port: port,
-//     },
-// };
 const bot = new Telegraf(process.env.TG_KEY as string);
 const telegram = new Telegram(process.env.TG_KEY as string);
 
-// BOT
-// bot.start((ctx) => {
-//     ctx.reply(`Hello  ${ctx.from.first_name}${ctx.from.last_name}!`);
-// });
-
-// bot.help((ctx) => {
-//     ctx.reply('restartDB, start');
-// });
-
-// bot.hears('restartDB', (ctx) => {
-//     initializeDb();
-//     ctx.reply('DB restarted');
-// });
-
-// const tryDate = new Date().toLocaleString('it-IT', { timeZone: 'Europe/Rome' });
-// console.log(tryDate);
-
-// RECURRENT RULE - SCHEDULED
-// const rule = new RecurrenceRule();
-// rule.hour = 7;
-// rule.minute = 0;
-// rule.second = 0;
-// rule.tz = `CET`;
-
-// new Date(year, monthIndex, day, hours, minutes, seconds)
-const startTime = new Date('2022-06-26T21:10:00.000+02:00').getTime();
+const startTime = new Date('2022-06-26T21:25:00.000+02:00').getTime();
 const endTime = new Date('2022-07-01T00:00:00.000+02:00').getTime();
-
-// console.log(Date.now());
-// console.log(startTime);
-
-// telegram.sendMessage(chatID, 'ciao');
-
-// const job = scheduleJob(rule, function () {
-//     // console.log('Today is recognized by Rebecca Black!');
-//     // telegram.sendMessage(chatID, `${new Date().toLocaleTimeString()}`);
-//     telegram.sendMessage(chatID, sendTurns());
-// });
 
 const job3 = scheduleJob(
     {
@@ -66,27 +25,6 @@ const job3 = scheduleJob(
         telegram.sendMessage(chatID, sendTurns());
     }
 );
-// function getDate() {
-//     const date = new Date();
-//     const str = date.toLocaleTimeString('it-IT');
-//     return str;
-// }
-
-// const job2 = scheduleJob('*/30 * * * * *', function () {
-//     // console.log('Today is recognized by Rebecca Black!');
-//     // console.log(tryDate);
-//     // telegram.sendMessage(chatID, `${new Date().toLocaleTimeString()}`);
-//     telegram.sendMessage(chatID, sendTurns());
-// });
-
-// ! LAUNCH AND END GRACEFULLY ON NODE STOP
-// bot.launch();
-// bot.launch({
-//     webhook: {
-//         domain: URL,
-//         port: port,
-//     },
-// });
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
