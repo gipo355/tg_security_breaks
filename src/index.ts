cbaaeb13e9554c14cdfee8bb3845f890add33448
gipo355 <gipo18649ah@gmail.com>
2022-06-26 22:19:20 +0200

    first push

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,15 @@ const chatID = process.env.CHAT_ID as string;
 const bot = new Telegraf(process.env.TG_KEY as string);
 const telegram = new Telegram(process.env.TG_KEY as string);
 
-const startTime = new Date('2022-07-01T00:00:00.000+02:00').getTime();
+// const startTime = new Date('2022-07-01T00:00:00.000+02:00').getTime();
+const startTime = new Date('2022-07-01T22:20:00.000+02:00').getTime();
 const endTime = new Date('2022-09-01T00:00:00.000+02:00').getTime();
 
 const job3 = scheduleJob(
     {
         start: startTime,
         end: endTime,
-        rule: '00 00 07 * * *',
+        rule: '05 * * * * *',
     },
     () => {
         telegram.sendMessage(chatID, sendTurns());
