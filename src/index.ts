b92fed50cfda9c7f69164757e5b7628fa78b5843
gipo355 <gipo18649ah@gmail.com>
2022-06-27 07:34:49 +0200

    for raffa

--- src/index.ts
+++ src/index.ts
@@ -10,18 +10,19 @@ const chatID = process.env.CHAT_ID as string;
 const bot = new Telegraf(process.env.TG_KEY as string);
 const telegram = new Telegram(process.env.TG_KEY as string);
 
-// const startTime = new Date('2022-07-01T00:00:00.000+02:00').getTime();
-const startTime = new Date('2022-06-01T00:00:00.000+02:00').getTime();
+const startTime = new Date('2022-07-01T00:00:00.000+02:00').getTime();
+// const startTime = new Date('2022-06-01T00:00:00.000+02:00').getTime();
 const endTime = new Date('2022-09-01T00:00:00.000+02:00').getTime();
 
 // TODO fix rule, startTime, chatid and starting DB + start dyno / dyno is gmt+0
 const job3 = scheduleJob(
     {
-        start: startTime,
-        end: endTime,
+        start: startTime, // milliseconds
+        end: endTime, // milliseconds
+        rule: '00 00 05 * * *',
         // rule: '05 * * * * *',
         // rule: '00 25 05 * * *',
-        rule: '*/2 * * * *',
+        // rule: '*/2 * * * *',
     },
     () => {
         telegram.sendMessage(chatID, sendTurns());
